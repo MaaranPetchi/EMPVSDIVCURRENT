a7d8e67adc01b512198934444fbd2050a421c011
maaran <maaranfamily22@gmail.com>
2023-03-17 18:32:19 +0530

    Empvsdiv

---
+++ src/app/EmployeeVSDivision/addeditemployeevsdivision/addeditemployeevsdivision.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-addeditemployeevsdivision',
+  templateUrl: './addeditemployeevsdivision.component.html',
+  styleUrls: ['./addeditemployeevsdivision.component.scss']
+})
+export class AddeditemployeevsdivisionComponent {
+
+}
--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AddeditemployeevsdivisionComponent } from './EmployeeVSDivision/addeditemployeevsdivision/addeditemployeevsdivision.component';
+import { indexemployeevsdivisionComponent } from './EmployeeVSDivision/indexemployeevsdivision/indexemployeevsdivision.component';
 
-const routes: Routes = [];
+const routes: Routes = [{ path: 'index', component:indexemployeevsdivisionComponent },
+{ path: 'edit', component: AddeditemployeevsdivisionComponent },];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
